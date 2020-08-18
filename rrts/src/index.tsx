1651b9d2e696791ef72c69d987c28254d56c8433
Ryuta Oshikiri <ryushikiri@gmail.com>
2020-08-17 21:48:01 -0700

    react class and function with interfaces

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render() {
-    return <div>Hi</div>
-  }
+interface AppProps {
+  color?: string;
 }
 
+interface AppState {
+  counter: number;
+}
+// functional component
+//const App = (props: AppProps): JSX.Element => {
+  //return (
+    //<div>
+      //{props.color}
+    //</div>
+  //);
+//};
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+// class component
+//class App extends React.Component <AppProps, AppState> {
+  //constructor(props: AppProps) {
+    //super(props);
+   //this.state = { counter: 0 };
+  //}
+  
+  //onIncrement = (): void => {
+    //this.setState({counter: this.state.counter + 1});
+  //}
+
+  //onDecrement = (): void => {
+    //this.setState({counter: this.state.counter - 1});
+  //}
+
+  //render() {
+    //const { color } = this.props;
+    //return (
+      //<div>
+        //<button onClick={this.onIncrement}>
+          //increment
+        //</button>
+        //<button onClick={this.onDecrement}>
+          //decrement
+        //</button>
+        //{this.state.counter}
+      //</div>
+    //)
+  //}
+//}
+
+
+ReactDOM.render(<App color='red' />, document.querySelector('#root'));
