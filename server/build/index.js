14c9846ecb4c97eb86493043402733b81c98a01c
Ryuta Oshikiri <ryushikiri@gmail.com>
2020-08-20 19:01:58 -0700

    simple express + typescript server

---
+++ server/build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var loginRoutes_1 = require("./routes/loginRoutes");
+var body_parser_1 = __importDefault(require("body-parser"));
+var app = express_1.default();
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(loginRoutes_1.router);
+app.get('/', function (req, res) {
+    res.send("\n    <div>\n    <h1>Base page</h1>\n    </div>\n  ");
+});
+app.listen(3000, function () {
+    console.log('listening 3000...');
+});
---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+    <h1>Base page</h1>
+    </div>
+  `)
+});
+
+app.listen(3000, () => {
+  console.log('listening 3000...');
+});
